acbe1725be3973e0099712637f1487d3b5f6989d
shrvya <shrivyashetty@gmail.com>
2021-09-29 12:25:28 +0530

    added:Employee Payroll Service

---
+++ src/main/java/com/employeepayroll/employeepayroll/EmployeePayrollData.java
@@ -0,0 +1,18 @@
+package com.employeepayroll.employeepayroll;
+
+
+public class EmployeePayrollData {
+   private int id;
+   private String name;
+   private double salary;
+   public EmployeePayrollData(Integer id,String name,Double salary)
+   {
+       this.id=id;
+       this.name=name;
+       this.salary=salary;
+   }
+   public String toString()
+   {
+       return "id="+id+",name"+'\''+",salary="+salary;
+   }
+}
